ff6f181bcaf593582470a4c734ecb220f7b4b1e5
Dwayne Slater <ds84182@gmail.com>
2013-08-02 14:47:15 -0400

    15 minute commit

--- autocommit.lua
+++ autocommit.lua
@@ -7,10 +7,10 @@
 	git push
 ]]
 function commit()
-	os.execute("rm assets -recurse")
-	os.execute("rm ds -recurse")
-	os.execute('xcopy -v "C:/Users/Dwayne/Games/MCForge/1.6.2_804/forge/mcp/src/minecraft/assets" ./assets /e /i /h')
-	os.execute('xcopy -v "C:/Users/Dwayne/Games/MCForge/1.6.2_804/forge/mcp/src/minecraft/ds" ./ds /e /i /h')
+	os.execute("rm assets")
+	os.execute("rm ds")
+	os.execute('xcopy -v "C:/Users/Dwayne/Games/MCForge/1.6.2_804/forge/mcp/src/minecraft/assets" . assets /e /i /h')
+	os.execute('xcopy -v "C:/Users/Dwayne/Games/MCForge/1.6.2_804/forge/mcp/src/minecraft/ds" . ds /e /i /h')
 	os.execute('git add .')
 	os.execute('git commit -m "15 minute commit"')
 	os.execute('git push')
