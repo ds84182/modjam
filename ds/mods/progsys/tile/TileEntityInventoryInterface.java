cd5e7c75bf418f034beec42ee6d81a447c97f271
Dwayne Slater <ds84182@gmail.com>
2013-08-02 17:00:37 -0400

    15 minute commit

--- ds/mods/progsys/tile/TileEntityInventoryInterface.java
+++ ds/mods/progsys/tile/TileEntityInventoryInterface.java
@@ -1,5 +1,7 @@
 package ds.mods.progsys.tile;
 
+import net.minecraft.inventory.IInventory;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.ForgeDirection;
 import ds.mods.progsys.wirednet.Network;
 import ds.mods.progsys.wirednet.Vector3;
@@ -7,20 +9,61 @@ import ds.mods.progsys.wirednet.drivers.ItemDriver;
 import ds.mods.progsys.wirednet.netbase.DriverNetworkBase;
 
 public class TileEntityInventoryInterface extends TileEntityNetworkBase {
+	public ForgeDirection facing;
+	public ItemDriver driver;
 
 	@Override
 	public boolean canBeAddedToNetwork(Network net, ForgeDirection side) {
-		return true;
+		return inventoryExists();
 	}
 
 	@Override
 	public void createNetworkBase(Network net) {
-		netbase = new DriverNetworkBase(new ItemDriver(null), new Vector3(xCoord,yCoord,zCoord));
+		driver = new ItemDriver(null);
+		netbase = new DriverNetworkBase(driver, new Vector3(xCoord,yCoord,zCoord));
 	}
 
 	@Override
 	public void createDefaultNetwork() {
 		net = new Network(worldObj);
+	}
+	
+	public boolean inventoryExists()
+	{
+		Vector3 invv = new Vector3(xCoord,yCoord,zCoord).add(facing);
+		TileEntity tile = worldObj.getBlockTileEntity(invv.x, invv.y, invv.z);
+		return tile != null && tile instanceof IInventory;
+	}
+	
+	public IInventory getInventory()
+	{
+		if (!inventoryExists()) return null;
+		Vector3 invv = new Vector3(xCoord,yCoord,zCoord).add(facing);
+		TileEntity tile = worldObj.getBlockTileEntity(invv.x, invv.y, invv.z);
+		return (IInventory) tile;
+	}
+	
+	@Override
+	public void updateEntity()
+	{
+		if (ForgeDirection.VALID_DIRECTIONS[getBlockMetadata()] != facing)
+		{
+			//Dir changed!
+			if (inventoryExists())
+			{
+				if (netbase != null)
+				{
+					driver.inv = getInventory();
+				}
+			}
+		}
+		else
+		{
+			if (driver.inv != getInventory())
+			{
+				driver.inv = getInventory();
+			}
+		}
 	}
 
 }
