8b5a2416211393449b3021ea5fecbe67cfe2f88c
Dwayne Slater <ds84182@gmail.com>
2013-08-02 14:38:05 -0400

    15 minute commit

--- autocommit.lua
+++ autocommit.lua
@@ -7,19 +7,19 @@
 	git push
 ]]
 function commit()
-	os.remove("assets")
-	os.remove("ds")
-	os.execute('cp "C:\Users\Dwayne\Games\MCForge\1.6.2_804\forge\mcp\src\minecraft\assets" assets')
-	os.execute('cp "C:\Users\Dwayne\Games\MCForge\1.6.2_804\forge\mcp\src\minecraft\ds" ds')
+	os.execute("rm -rf assets")
+	os.execute("rm -rf ds")
+	os.execute('cp "C:/Users/Dwayne/Games/MCForge/1.6.2_804/forge/mcp/src/minecraft/assets" assets')
+	os.execute('cp "C:/Users/Dwayne/Games/MCForge/1.6.2_804/forge/mcp/src/minecraft/ds" ds')
 	os.execute('git add .')
 	os.execute('git commit -m "15 minute commit"')
 	os.execute('git push')
 end
 
 while true do
+	commit()
 	local lasttime = os.time()
 	while os.time()-lasttime < 60*15 do
 		
 	end
-	commit()
 end
