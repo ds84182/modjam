1808b15baffdbd450bf0512273a5faf85087cc73
Dwayne Slater <ds84182@gmail.com>
2013-08-02 21:21:17 -0400

    15 minute commit

--- ds/mods/progsys/client/holo/HoloInventoryInterface.java
+++ ds/mods/progsys/client/holo/HoloInventoryInterface.java
@@ -4,8 +4,11 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
 
 import org.lwjgl.opengl.GL11;
+
+import com.google.common.primitives.SignedBytes;
 
 import ds.mods.progsys.tile.TileEntityInventoryInterface;
 
@@ -22,6 +25,21 @@ public class HoloInventoryInterface extends HoloGui {
 				return false;
 			}
 			
+			@Override
+			public boolean shouldSpreadItems() {
+                return false;
+            }
+			
+			@Override
+            public byte getMiniBlockCount(ItemStack stack) {
+                return SignedBytes.saturatedCast(Math.min(stack.stackSize / 32, 15) + 1);
+            }
+			
+            @Override
+            public byte getMiniItemCount(ItemStack stack) {
+                return SignedBytes.saturatedCast(Math.min(stack.stackSize / 32, 7) + 1);
+            }
+			
 		};
 		render.setRenderManager(RenderManager.instance);
 	}
@@ -31,26 +49,6 @@ public class HoloInventoryInterface extends HoloGui {
 		//We will render all the items at once, but firs we need to find a number that is square from the stack number
 		if (tile.invInfo != null)
 		{
-			
-		}
-	}
-
-	@Override
-	public void draw() {
-		GL11.glPushMatrix();
-		GL11.glScalef(2F, 2F, 2F);
-		drawString(font,"Inventory", 0, 0, 0xFFFFFF);
-		drawString(font,"Interface", 0, 8, 0xFFFFFF);
-		GL11.glPopMatrix();
-		if (tile.invInfo != null)
-		{
-			int numOfItems = 0;
-			for (int i = 0; i<tile.invInfo.size; i++)
-			{
-				//tile.driver.inv.
-				if (tile.invInfo.stacks[i] != null) numOfItems++;
-			}
-			drawString(font,"Items: "+numOfItems+"/"+tile.invInfo.size, 3, 32, 0xFFFFFF);
 			int siz = tile.invInfo.size;
 			double sq = Math.sqrt(siz);
 			while (Math.floor(sq) != sq)
@@ -75,7 +73,8 @@ public class HoloInventoryInterface extends HoloGui {
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glColor4f(0.1F, 0.4F, 0.3F, 0.5F);
 			int stackAt = 0;
-			EntityItem item = new EntityItem(Minecraft.getMinecraft().theWorld,0D,0D,0D);
+			EntityItem item = new EntityItem(Minecraft.getMinecraft().theWorld);
+			item.hoverStart = 0f;
 			for (int cx = 0; cx<sq; cx++)
 			{
 				for (int cy = 0; cy<sq; cy++)
@@ -84,10 +83,9 @@ public class HoloInventoryInterface extends HoloGui {
 					if (stackAt<tile.invInfo.stacks.length && tile.invInfo.stacks[stackAt] != null)
 					{
 						GL11.glEnable(GL11.GL_TEXTURE_2D);
-						//GL11.glDisable(GL11.GL_BLEND);
+						GL11.glDisable(GL11.GL_BLEND);
 						item.setEntityItemStack(tile.invInfo.stacks[stackAt]);
-						render.renderItemIntoGUI(font, Minecraft.getMinecraft().renderEngine, tile.invInfo.stacks[stackAt], cx*(w+2), cy*(h+2));
-						render.renderItemOverlayIntoGUI(font, Minecraft.getMinecraft().renderEngine, tile.invInfo.stacks[stackAt], cx*(w+2), cy*(h+2));
+						render.doRenderItem(item, 0,0,0,0,0);
 						GL11.glDisable(GL11.GL_TEXTURE_2D);
 						GL11.glEnable(GL11.GL_BLEND);
 					}
@@ -97,6 +95,26 @@ public class HoloInventoryInterface extends HoloGui {
 			}
 			GL11.glEnable(GL11.GL_TEXTURE_2D);
 			GL11.glDisable(GL11.GL_BLEND);
+		}
+	}
+
+	@Override
+	public void draw() {
+		GL11.glPushMatrix();
+		GL11.glScalef(2F, 2F, 2F);
+		drawString(font,"Inventory", 0, 0, 0xFFFFFF);
+		drawString(font,"Interface", 0, 8, 0xFFFFFF);
+		GL11.glPopMatrix();
+		if (tile.invInfo != null)
+		{
+			int numOfItems = 0;
+			for (int i = 0; i<tile.invInfo.size; i++)
+			{
+				//tile.driver.inv.
+				if (tile.invInfo.stacks[i] != null) numOfItems++;
+			}
+			drawString(font,"Items: "+numOfItems+"/"+tile.invInfo.size, 3, 32, 0xFFFFFF);
+			
 		}
 	}
 
