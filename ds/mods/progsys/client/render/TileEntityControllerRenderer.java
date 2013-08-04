1ae609fd11e56516b660ad3b6f0c04f4741cb797
Dwayne Slater <ds84182@gmail.com>
2013-08-03 23:39:33 -0400

    15 minute commit

--- ds/mods/progsys/client/render/TileEntityControllerRenderer.java
+++ ds/mods/progsys/client/render/TileEntityControllerRenderer.java
@@ -1,21 +1,43 @@
 package ds.mods.progsys.client.render;
 
-import net.minecraft.block.material.Material;
+import java.nio.FloatBuffer;
+
+import net.minecraft.client.renderer.GLAllocation;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Vec3;
 
 import org.lwjgl.opengl.GL11;
 
 import ds.mods.progsys.client.holo.HoloController;
 import ds.mods.progsys.client.holo.HoloUtils;
+import ds.mods.progsys.client.model.ModelCrystal;
 import ds.mods.progsys.tile.TileEntityController;
 
 public class TileEntityControllerRenderer extends TileEntitySpecialRenderer {
-
+	private ModelCrystal model = new ModelCrystal();
+	private static FloatBuffer colorBuffer = GLAllocation.createDirectFloatBuffer(4);
+	private static final Vec3 field_82884_b = Vec3.createVectorHelper(0.20000000298023224D, 1.0D, -0.699999988079071D).normalize();
+    private static final Vec3 field_82885_c = Vec3.createVectorHelper(-0.20000000298023224D, 1.0D, 0.699999988079071D).normalize();
+	
 	@Override
 	public void renderTileEntityAt(TileEntity tileentity, double d0, double d1,
 			double d2, float f) {
 		TileEntityController tile = (TileEntityController) tileentity;
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glPushMatrix();
+		{
+			GL11.glTranslated(d0+0.5D, d1+0.5D, d2+1D);
+			GL11.glRotated(180D, 0.132113D, 0.4123D, 0.5931D);
+			GL11.glColor3f(0F, 0.64F, 0.022558F);
+			enableStandardItemLighting();
+			GL11.glShadeModel(GL11.GL_SMOOTH);
+			//GL11.glMaterial(face, pname, params)
+			model.render();
+			disableStandardItemLighting();
+		}
+		GL11.glPopMatrix();
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		if (tile.showHolo)
 		{
 			GL11.glPushMatrix();
@@ -29,6 +51,42 @@ public class TileEntityControllerRenderer extends TileEntitySpecialRenderer {
 		}
 	}
 	
+	private static FloatBuffer setColorBuffer(float par0, float par1, float par2, float par3)
+    {
+        colorBuffer.clear();
+        colorBuffer.put(par0).put(par1).put(par2).put(par3);
+        colorBuffer.flip();
+        return colorBuffer;
+    }
 	
+	public static void enableStandardItemLighting()
+    {
+        GL11.glEnable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_LIGHT0);
+        GL11.glEnable(GL11.GL_LIGHT1);
+        GL11.glEnable(GL11.GL_COLOR_MATERIAL);
+        GL11.glColorMaterial(GL11.GL_FRONT_AND_BACK, GL11.GL_AMBIENT_AND_DIFFUSE);
+        float f = 0.4F;
+        float f1 = 0.6F;
+        float f2 = 0.0F;
+        GL11.glLight(GL11.GL_LIGHT0, GL11.GL_POSITION, setColorBuffer((float)field_82884_b.xCoord, (float)field_82884_b.yCoord, (float)field_82884_b.zCoord, 0.0F));
+        GL11.glLight(GL11.GL_LIGHT0, GL11.GL_DIFFUSE, setColorBuffer(f1, f1, f1, 1.0F));
+        GL11.glLight(GL11.GL_LIGHT0, GL11.GL_AMBIENT, setColorBuffer(0.0F, 0.0F, 0.0F, 1.0F));
+        GL11.glLight(GL11.GL_LIGHT0, GL11.GL_SPECULAR, setColorBuffer(f2, f2, f2, 1.0F));
+        GL11.glLight(GL11.GL_LIGHT1, GL11.GL_POSITION, setColorBuffer((float)field_82885_c.xCoord, (float)field_82885_c.yCoord, (float)field_82885_c.zCoord, 0.0F));
+        GL11.glLight(GL11.GL_LIGHT1, GL11.GL_DIFFUSE, setColorBuffer(f1, f1, f1, 1.0F));
+        GL11.glLight(GL11.GL_LIGHT1, GL11.GL_AMBIENT, setColorBuffer(0.0F, 0.0F, 0.0F, 1.0F));
+        GL11.glLight(GL11.GL_LIGHT1, GL11.GL_SPECULAR, setColorBuffer(f2, f2, f2, 1.0F));
+        GL11.glShadeModel(GL11.GL_FLAT);
+        GL11.glLightModel(GL11.GL_LIGHT_MODEL_AMBIENT, setColorBuffer(f, f, f, 1.0F));
+    }
+	
+	public static void disableStandardItemLighting()
+    {
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_LIGHT0);
+        GL11.glDisable(GL11.GL_LIGHT1);
+        GL11.glDisable(GL11.GL_COLOR_MATERIAL);
+    }
 
 }
