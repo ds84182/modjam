227ee4ae326ae760b07bc9aeb17389ed1fe759ce
Dwayne Slater <ds84182@gmail.com>
2013-08-04 13:36:53 -0400

    15 minute commit

--- ds/mods/progsys/client/holo/ItemHolos.java
+++ ds/mods/progsys/client/holo/ItemHolos.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
 import ds.mods.progsys.items.ItemPS;
@@ -14,13 +15,14 @@ public class ItemHolos extends ItemPS {
 
 	public ItemHolos(int par1) {
 		super(par1);
+		this.hasSubtypes = true;
 	}
 
 	@Override
 	public String getItemDisplayName(ItemStack par1ItemStack) {
 		return par1ItemStack.getItemDamage() == 0 ? LanguageRegistry.instance().getStringLocalization("item.ds.progsys.holos.pixel.name") : LanguageRegistry.instance().getStringLocalization("item.ds.progsys.holos.array.name");
 	}
-	
+
 	@Override
 	@SideOnly(Side.CLIENT)
 	public Icon getIconFromDamage(int par1) {
